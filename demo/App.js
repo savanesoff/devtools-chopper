2ad22e200c3ab5623ca9dcdef14e5f00c830c890
Sam Avanesov <sam.avanesoff@gmail.com>
2023-07-09 12:56:01 -0400

    Updates

--- demo/App.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx, Fragment as _Fragment, jsxs as _jsxs } from "react/jsx-runtime";
-import "./App.css";
-import "./chopper";
-function App() {
-    return (_jsxs(_Fragment, { children: [_jsx("h1", { children: "devtools-chopper" }), _jsx("p", { children: "Chopper is a simple, fast and flexible log control tool designed display logs in browser window with various filters, drag and resize capabilities." }), _jsx("div", {})] }));
-}
-export default App;
