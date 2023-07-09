0266f3dba5cabc784a8ff2e8adfb700c100003c9
Sam Avanesov <sam.avanesoff@gmail.com>
2023-07-09 10:23:56 -0400

    Build config setup

--- demo/App.tsx
+++ demo/App.tsx
@@ -14,17 +14,6 @@ function App() {
         capabilities.
       </p>
       <div></div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
