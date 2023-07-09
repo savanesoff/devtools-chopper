ac46785dc7440d8fb4a559218f70f6e2d1664053
Sam Avanesov <sam.avanesoff@gmail.com>
2023-07-09 13:05:25 -0400

    Demo build  and deployment configuration

--- demo/App.tsx
+++ demo/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import "./App.css";
 import "./chopper";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>devtools-chopper</h1>
