b01a5dfb09cb17fd31a9e19d8ee691689a880a95
Sam Avanesov <sam.avanesoff@gmail.com>
2023-07-11 13:40:33 -0400

    Set readme file path to gh raw file path

--- demo/App.tsx
+++ demo/App.tsx
@@ -2,11 +2,18 @@ import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import "./App.css";
 import "./chopper";
+// determine the environment
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore - this is a global variable
+const isDev = process.env.NODE_ENV === "development";
+const readmePath = isDev
+  ? "../README.md"
+  : "https://raw.githubusercontent.com/savanesoff/devtools-chopper/main/README.md";
 function App() {
   const [content, setContent] = useState("");
 
   useEffect(() => {
-    fetch("../README.md")
+    fetch(readmePath, { mode: "no-cors" })
       .then((res) => res.text())
       .then((text) => setContent(text));
   }, []);
