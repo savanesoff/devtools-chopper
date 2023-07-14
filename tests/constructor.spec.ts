cdb58091cf4de573e14a3410550f819e67806a13
Sam Avanesov <sam.avanesoff@gmail.com>
2023-07-14 11:38:43 -0400

    Constructor tests

--- tests/constructor.spec.ts
+++ tests/constructor.spec.ts
@@ -1,70 +1,72 @@
 import { screen } from "@testing-library/dom";
 import "vitest-dom/extend-expect";
 import Chopper from "./../src";
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-function isNullOrUndefined(value: unknown): boolean {
-  return value === null || value === undefined;
-}
+import Overdrag, { ControlProps } from "overdrag";
+import { getRandomValue } from "./setup";
 
-/**
- * Mock HTMLElement.offsetParent as it is not supported in JEST DOM
- */
-Object.defineProperty(HTMLElement.prototype, "offsetParent", {
-  get() {
-    // eslint-disable-next-line @typescript-eslint/no-this-alias
-    let element = this;
-    while (
-      !isNullOrUndefined(element) &&
-      (isNullOrUndefined(element.style) ||
-        isNullOrUndefined(element.style.display) ||
-        element.style.display.toLowerCase() !== "none")
-    ) {
-      element = element.parentNode;
-    }
-
-    if (!isNullOrUndefined(element)) {
-      return null;
-    }
-
-    if (
-      !isNullOrUndefined(this.style) &&
-      !isNullOrUndefined(this.style.position) &&
-      this.style.position.toLowerCase() === "fixed"
-    ) {
-      return null;
-    }
-
-    if (
-      this.tagName.toLowerCase() === "html" ||
-      this.tagName.toLowerCase() === "body"
-    ) {
-      return null;
-    }
-
-    return this.parentNode;
-  },
+afterEach(() => {
+  // Reset mock function calls
+  vi.clearAllMocks();
 });
 
-export function getRandomValue(min = 0, max = 50) {
-  return min + Math.round(Math.random() * (max - min));
-}
-
-export function getRandomPixelValue(min = 0, max = 50) {
-  return `${getRandomValue(min, max)}px`;
-}
-
 describe("Constructor", () => {
-  afterEach(() => {
-    // Reset mock function calls
-    vi.clearAllMocks();
-  });
-
-  it(`Should be defined`, () => {
+  it(`should be defined`, () => {
     expect(Chopper).toBeDefined();
   });
 
-  it("Should render chopper", () => {
+  it("should render chopper", () => {
     new Chopper({ name: "Super name" });
-    expect(screen.getByText("Super name")).toBeInTheDocument();
+    expect(screen.queryByText("Super name")).toBeInTheDocument();
+  });
+
+  it('should have log level "verbose" by default', () => {
+    const chopper = new Chopper({ name: "Super name" });
+    expect(chopper.getLogLevel()).toBe("verbose");
+  });
+
+  it("Should initialize super with parameters", () => {
+    const element = document.createElement("div");
+    globalThis.document.body.appendChild(element);
+    const overdragProps: Partial<ControlProps> = {
+      element,
+      maxContentHeight: getRandomValue(),
+      maxContentWidth: getRandomValue(),
+      minContentHeight: getRandomValue(),
+      minContentWidth: getRandomValue(),
+      snapThreshold: getRandomValue(),
+      controlsThreshold: getRandomValue(),
+      clickDetectionThreshold: getRandomValue(),
+    };
+    const chopper = new Chopper({ name: "Super name", ...overdragProps });
+    expect(chopper.element).toBe(overdragProps.element);
+    expect(chopper.maxContentHeight).toBe(overdragProps.maxContentHeight);
+    expect(chopper.maxContentWidth).toBe(overdragProps.maxContentWidth);
+    expect(chopper.minContentHeight).toBe(overdragProps.minContentHeight);
+    expect(chopper.minContentWidth).toBe(overdragProps.minContentWidth);
+    expect(chopper.snapThreshold).toBe(overdragProps.snapThreshold);
+    expect(chopper.controlsThreshold).toBe(overdragProps.controlsThreshold);
+    expect(chopper.clickDetectionThreshold).toBe(
+      overdragProps.clickDetectionThreshold
+    );
+  });
+});
+
+describe("APIs", () => {
+  let chopper: Chopper;
+  beforeEach(() => {
+    chopper = new Chopper({ name: "Super name" });
+  });
+
+  describe("getLogLevel", () => {
+    it("should return log level", () => {
+      expect(chopper.getLogLevel()).toBe("verbose");
+    });
+  });
+
+  describe("setLogLevel", () => {
+    it("should set log level", () => {
+      chopper.setLogLevel("error");
+      expect(chopper.getLogLevel()).toBe("error");
+    });
   });
 });
